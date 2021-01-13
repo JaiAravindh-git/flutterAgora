025c7b7526d79c3e49e166c77f449d35caa6b995
JaiAravindh-git <offl.aravindh@gmail.com>
2021-01-13 09:40:50 +0530

    AllFiles

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const APP_ID = "160442d7dfe24de18252f872322a9ddb";
+const Token =
+    "006160442d7dfe24de18252f872322a9ddbIADn03sA1zkwKtSeY3fon6JUiYhhLCqvR/F3bXLhbDFvfMJBJDUAAAAAEACZqwdIq7z/XwEAAQCpvP9f";
